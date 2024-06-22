39d732fe1a547b960f19e3232838399b26fa23cd
Sahin Dursun <sahin.dursun@relexsolutions.com>
2024-06-22 15:06:11 +0200

    init commit

---
+++ docker_sql/ingest_data.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+import os
+import argparse
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+
+    parquet_name = "output.parquet"
+
+    os.system(f"wget {url} -O {parquet_name}")
+
+    ## download the parquet
+    df = pd.read_parquet("output.parquet")
+
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
+
+    engine.connect()
+
+    df.head(n=0).to_sql(name="yellow_taxi_data", con=engine, if_exists="replace")
+
+    print(pd.io.sql.get_schema(df, name=table_name, con=engine))
+
+    df.to_sql(name=table_name, con=engine, if_exists="replace")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="I")
+    parser.add_argument("--user", help="user for postgres")
+    parser.add_argument("--password", help="password for postgres")
+    parser.add_argument("--host", help="hostname for postgres")
+    parser.add_argument("--port", help="port")
+    parser.add_argument("--db", help="database name")
+    parser.add_argument("--table_name", help="table name for data storage")
+    parser.add_argument("--url", help="url of the parquet file")
+
+    args = parser.parse_args()
+
+    main(args)
